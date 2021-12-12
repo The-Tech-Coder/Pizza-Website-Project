d6d54e3b8aad6f160812cd39218b9935345de11e
The Tech Coder <91053240+The-Tech-Coder@users.noreply.github.com>
2021-12-12 09:49:09 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzas de Deliciosas</title>
+    <link rel="stylesheet" href="contact.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+        </header>
+    
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="menu.php">Menu</a>
+            <a href="gift.php">Gift Voucher</a>
+            <a href="catering.php">Catering</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    
+        <main>
+            <h1>Eat at our Pizzeria</h1>
+            <p>
+                <table>
+                    <tr>
+                        <td>Shop Name</td>
+                        <td>Pizzas de Deliciosas</td>
+                    </tr>
+                    <tr>
+                        <td>email</td>
+                        <td>PDD@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td>phone</td>
+                        <td>123456789</td>
+                    </tr>
+                    <tr>
+                        <td>Our Other Branches</td>
+                        <td>Location</td>
+                    </tr>
+                    <tr>
+                        <td>Pizzas de Famosas</td>
+                        <td>Ocean City</td>
+                    </tr>
+                    <tr>
+                        <td>Amor de pizza</td>
+                        <td>Sun City</td>
+                    </tr>
+                </table>
+            </p>
+            <a href="pizzanew.php">Order Online</a>
+        </main>
+    
+        <footer>
+            <p>Westview, Encanto City, Naples - Copyright ©™ 1947</p>
+        </footer>
+    </div>
+</body>
+</html>
