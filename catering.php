2c39468cc1fec43aee618a2698f8b3d3102aee58
The Tech Coder <91053240+The-Tech-Coder@users.noreply.github.com>
2021-12-12 09:47:00 +0530

    Create catering.php

---
+++ catering.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzas de Deliciosas</title>
+    <link rel="stylesheet" href="pizza.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+        </header>
+    
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="menu.php">Menu</a>
+            <a href="gift.php">Gift Voucher</a>
+            <a href="catering.php">Catering</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    
+        <main>
+            <h1>Eat at our Pizzeria</h1>
+            <p>
+                We provide Catering Facilities for large Gatherings, providing a range of menus in affordable prices.
+            </p>
+            <a href="pizzanew.php">Order Online</a>
+        </main>
+    
+        <footer>
+            <p>Westview, Encanto City, Naples - Copyright ©™ 1947</p>
+        </footer>
+    </div>
+</body>
+</html>
