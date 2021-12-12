7b2fe4d79ede5a63be0522821f6dc222d7e1ef6b
The Tech Coder <91053240+The-Tech-Coder@users.noreply.github.com>
2021-12-12 09:50:01 +0530

    Create gift.php

---
+++ gift.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzas de Deliciosas</title>
+    <link rel="stylesheet" href="gift.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+        </header>
+    
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="menu.php">Menu</a>
+            <a href="gift.php">Gift Voucher</a>
+            <a href="catering.php">Catering</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    
+        <main>
+            <table>
+                <tr>
+                    <th>Coupon Codes</th>
+                    <th>Burgers - 70rs</th>
+                    <th>Coke - 40rs</th>
+                    <th>Dessert - 60rs</th>
+                    <th>Fries - 50rs</th>
+                    <th>Today's special - 50rs</th>
+                    <th>Discount</th>
+                </tr>
+                <tr>
+                    <th>Veg Pizza</th>
+                    <th>B1</th>
+                    <th>C1</th>
+                    <th>D1</th>
+                    <th>F1</th>
+                    <th>T1</th>
+                    <th>Flat 15%</th>
+                </tr>
+                <tr>
+                    <th>Non-Veg Pizza</th>
+                    <th>B2</th>
+                    <th>C2</th>
+                    <th>D2</th>
+                    <th>F2</th>
+                    <th>T2</th>
+                    <th>Flat 20%</th>
+                </tr>
+            </table>
+            <a href="pizzanew.php">Order Online</a>
+        </main>
+    
+        <footer>
+            <p>Westview, Encanto City, Naples - Copyright ©™ 1947</p>
+        </footer>
+    </div>
+</body>
+</html>
