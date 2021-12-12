dc4ede85f01b9146253011715ba9d6ff9489ae81
The Tech Coder <91053240+The-Tech-Coder@users.noreply.github.com>
2021-12-12 09:50:55 +0530

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzas de Deliciosas</title>
+    <link rel="stylesheet" href="pizza.css">
+</head>
+<body>
+    <!-- <img src="/pizza1.jpg" alt=""> -->
+    <div class="container">
+        <header>
+        </header>
+    
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="menu.php">Menu</a>
+            <a href="gift.php">Gift Voucher</a>
+            <a href="catering.php">Catering</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    
+        <main>
+            <table>
+                <tr>
+                    <th>Veg</th>
+                    <th>small/medium/large</th>
+                    <th>Non-Veg</th>
+                    <th>small/medium/large</th>
+                </tr>
+                <tr>
+                    <td>Margherita </td>
+                    <td>99/129/169</td>
+                    <td>Chicken Sausage</td>
+                    <td>119/159/199</td>
+                </tr>
+                <tr>
+                    <td>Double Cheese</td>
+                    <td>89/119/159</td>
+                    <td>Chicken Alfredo</td>
+                    <td>129/169/209</td>
+                </tr>
+                <tr>
+                    <td>Spicy Tofu</td>
+                    <td>99/129/169</td>
+                    <td>Chicken Fiesta</td>
+                    <td>119/159/199</td>
+                </tr>
+                <tr>
+                    <td>Cheese N Corn</td>
+                    <td>89/119/159</td>
+                    <td>Barbecue</td>
+                    <td>129/169/209</td>
+                </tr>
+                <tr>
+                    <td>Mushroom</td>
+                    <td>99/129/169</td>
+                    <td>Pickled Chicken</td>
+                    <td>119/159/199</td>
+                </tr>
+                <tr>
+                    <td>Mix veggie</td>
+                    <td>89/119/159</td>
+                    <td>Oyester</td>
+                    <td>129/169/209</td>
+                </tr>
+                <tr>
+                    <td>Creepy Mayo</td>
+                    <td>99/129/169</td>
+                    <td>Shrimp</td>
+                    <td>119/159/199</td>
+                </tr>
+                <b><h3><a href="pizzanew.php">Order Online</a></h3></b>
+            </table>
+        </main>
+    
+        <footer>
+            <p>Westview, Encanto City, Naples - Copyright ©™ 1947</p>
+        </footer>
+    </div>
+</body>
+</html>
