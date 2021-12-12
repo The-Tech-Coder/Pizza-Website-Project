63e6683b78d0306cbb6d3230dda0a698931e042b
The Tech Coder <91053240+The-Tech-Coder@users.noreply.github.com>
2021-12-12 09:50:28 +0530

    Create home.php

---
+++ home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzas de Deliciosas</title>
+    <link rel="stylesheet" href="pizza.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+        </header>
+    
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="menu.php">Menu</a>
+            <a href="gift.php">Gift Voucher</a>
+            <a href="catering.php">Catering</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    
+        <main>
+            <h1>Eat at our Pizzeria</h1>
+                    <p>We love sharing good food with great people. We bring over 30 years industry experience and passion. With dishes that
+                       are carefully designed to bring you a satisfying Italian food experience that you are sure to remember.
+                    </p>
+            <a href="pizzanew.php">Order Online</a>
+        </main>
+    
+        <footer>
+            <p>Westview, Encanto City, Naples - Copyright ©™ 1947</p>
+        </footer>
+    </div>
+</body>
+</html>
